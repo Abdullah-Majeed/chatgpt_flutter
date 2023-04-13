96621159eb883bfc4a6929da1de9f226a04f3090
Abdullah Majeed <abdullahmajeed969@gmail.com>
2023-04-13 18:44:05 +0500

    chat gpt api integration with flutter

---
+++ lib/constants/api_const.dart
@@ -0,0 +1,2 @@
+String BASE_URL = "https://api.openai.com/v1";
+String API_KEY = "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";
---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,162 @@
+import 'dart:developer';
+
+import 'package:chatgpt_flutter/constants/constants.dart';
+import 'package:chatgpt_flutter/models/chat_model.dart';
+import 'package:chatgpt_flutter/providers/chat_provider.dart';
+import 'package:chatgpt_flutter/providers/models_provider.dart';
+import 'package:chatgpt_flutter/services/api_service.dart';
+import 'package:chatgpt_flutter/services/services.dart';
+import 'package:chatgpt_flutter/widgets/chat_widget.dart';
+import 'package:chatgpt_flutter/widgets/text_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:provider/provider.dart';
+
+import '../services/assets_manager.dart';
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  bool isTyping = false;
+  late TextEditingController textEditingController;
+  // List<ChatModel> chatList = [];
+  late FocusNode focusNode;
+  late ScrollController _scrollController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    textEditingController = TextEditingController();
+    _scrollController = ScrollController();
+    focusNode = FocusNode();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    textEditingController.dispose();
+    _scrollController.dispose();
+    focusNode.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final modelsProvider = Provider.of<ModelsProvider>(context, listen: false);
+    final chatListProvider = Provider.of<ChatProvider>(context, listen: false);
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+              onPressed: () async {
+                await Services.showBottomModalSheet(context: context);
+              },
+              icon: const Icon(Icons.more_vert_rounded, color: Colors.white))
+        ],
+        elevation: 2,
+        leading: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset(AssetsManager.openaiLogo)),
+        title: const Text('ChatGPT'),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Flexible(
+              child: ListView.builder(
+                  controller: _scrollController,
+                  itemCount:
+                      chatListProvider.getChatList.length, //chatList.length,
+                  itemBuilder: (context, index) {
+                    return ChatWidget(
+                        msg: chatListProvider
+                            .getChatList[index].msg, //chatList[index].msg,
+                        chatIndex: chatListProvider.getChatList[index]
+                            .chatIndex //chatList[index].chatIndex,
+                        );
+                  }),
+            ),
+            if (isTyping) ...[
+              const SpinKitThreeBounce(
+                color: Colors.white,
+                size: 18.0,
+              ),
+            ],
+            const SizedBox(
+              height: 15.0,
+            ),
+            Material(
+              color: cardColor,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                        child: TextField(
+                      focusNode: focusNode,
+                      style: const TextStyle(color: Colors.white),
+                      controller: textEditingController,
+                      onSubmitted: (value) async {
+                        await sendMessageFCT(modelsProvider, chatListProvider);
+                      },
+                      decoration: const InputDecoration.collapsed(
+                          hintText: 'How can I help you',
+                          hintStyle: TextStyle(color: Colors.grey)),
+                    )),
+                    IconButton(
+                      onPressed: () async {
+                        await sendMessageFCT(modelsProvider, chatListProvider);
+                      },
+                      icon: const Icon(Icons.send),
+                      color: Colors.white,
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void scrollListToEnd() {
+    _scrollController.animateTo(_scrollController.position.maxScrollExtent,
+        duration: const Duration(seconds: 2), curve: Curves.easeOut);
+  }
+
+  Future<void> sendMessageFCT(
+      ModelsProvider modelsProvider, ChatProvider chatListProvider) async {
+    try {
+      print("text: ${textEditingController.text}");
+      setState(() {
+        isTyping = true;
+        chatListProvider.addUserMessage(msg: textEditingController.text);
+        // chatList.add(ChatModel(msg: textEditingController.text, chatIndex: 0));
+        textEditingController.clear();
+        focusNode.unfocus();
+      });
+      await chatListProvider.sendMessageAndGetAnswers(chosenModelId: modelsProvider.getCurrentModel,msg: textEditingController.text);
+      // chatList.addAll(await ApiService.sendMessageGPT(
+      //     message: textEditingController.text,
+      //     modelId: modelsProvider.getCurrentModel));
+      print("after");
+      setState(() {});
+    } catch (e) {
+      log('$e');
+    } finally {
+      setState(() {
+        isTyping = false;
+        scrollListToEnd();
+      });
+    }
+  }
+}
